9141aa6b8e22c5510b2c00a308c0c1fc74ff3c32
WonbeenLee-bskr <leewonbeen@buysellstandards.com>
2024-04-15 17:43:26 +0900

    add : Post 클래스 추가

---
+++ src/main/java/com/simpleboard/demo/Post.java
@@ -0,0 +1,22 @@
+package com.simpleboard.demo;
+
+public class Post {
+    private String author;
+    private String content;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
